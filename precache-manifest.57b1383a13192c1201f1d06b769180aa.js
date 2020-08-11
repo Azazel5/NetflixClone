653408bce94488f6f0a810ad159bcb2a98fce0a6
Azazel5 <subhanga2013@gmail.com>
2020-08-11 07:05:35 -0400

    Updates

---
+++ precache-manifest.57b1383a13192c1201f1d06b769180aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32980c53e86b743df9b3c6c5bad1ffb4",
+    "url": "/NetflixClone/index.html"
+  },
+  {
+    "revision": "02988553246853b02abd",
+    "url": "/NetflixClone/static/css/main.55df4a06.chunk.css"
+  },
+  {
+    "revision": "eca04c74cc26640cd9ad",
+    "url": "/NetflixClone/static/js/2.0d69fbaa.chunk.js"
+  },
+  {
+    "revision": "02988553246853b02abd",
+    "url": "/NetflixClone/static/js/main.35ea439d.chunk.js"
+  },
+  {
+    "revision": "ac96cd940339047c5bc0",
+    "url": "/NetflixClone/static/js/runtime~main.45512f5e.js"
+  },
+  {
+    "revision": "149a098f51438682ddb64f6e0c0c709b",
+    "url": "/NetflixClone/static/media/landingPage.149a098f.jpg"
+  },
+  {
+    "revision": "8562b6565f5ae1db5e4af40d85b4ed2d",
+    "url": "/NetflixClone/static/media/netflix.8562b656.png"
+  },
+  {
+    "revision": "991556c2526eb97f8e745b79363c8661",
+    "url": "/NetflixClone/static/media/normal.991556c2.jpg"
+  },
+  {
+    "revision": "c2b89779dd6b85078fc0cc2f3a9aaf28",
+    "url": "/NetflixClone/static/media/profile.c2b89779.jpg"
+  }
+]);
